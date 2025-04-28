ac5a0a8cd290f71a48da25659cb094a5cbe03783
sor12345QWE <mkb17119810001@163.com>
2025-04-28 23:52:07 +0800

    Update nfc_14a.c

--- firmware/application/src/rfid/nfctag/hf/nfc_14a.c
+++ firmware/application/src/rfid/nfctag/hf/nfc_14a.c
@@ -624,8 +624,8 @@ void nfc_tag_14a_event_callback(nrfx_nfct_evt_t const *p_event) {
         case NRFX_NFCT_EVT_RX_FRAMEEND: {
             set_slot_light_color(RGB_YELLOW);
             TAG_FIELD_LED_ON()
-
-            config.button_b_press = SettingsButtonCycleSlotDec;
+ config.button_b_press = SettingsButtonCycleSlotDec;
+            
             
             // NRF_LOG_INFO("RX FRAMEEND.\n");
             // TODO Remember a bug, if you do not reply to the message after receiving the message, you need to manually enable you
